dc34079bce90470b22b4b66f9436b9afd6cdea67
its-kayes <kayes.ek8@gmail.com>
2022-05-22 19:12:30 +0600

    Modal install

--- src/page/Order/Purchase.js
+++ src/page/Order/Purchase.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import OrderModal from './OrderModal';
 
 const Purchase = () => {
 
@@ -28,7 +29,21 @@ const Purchase = () => {
                                 <p>Minimum buy: {part?.minOrder} pieces</p>
                             </div>
                             <p class="py-6"> {part?.description} </p>
-                            <button class="btn btn-primary">Get Started</button>
+                            <button > <label for="my-modal-6" class="btn modal-button">Order Now</label> </button>
+
+                            <div>
+                                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                                <div class="modal modal-bottom sm:modal-middle">
+                                    <div class="modal-box">
+                                        <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+                                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                        <div class="modal-action">
+                                            <label for="my-modal-6" class="btn">Yay!</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
