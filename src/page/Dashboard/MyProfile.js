c7aec9c7b434d5c551ef2377bd504aa4093f902a
its-kayes <kayes.ek8@gmail.com>
2022-05-23 18:06:09 +0600

    My profile done

--- src/page/Dashboard/MyProfile.js
+++ src/page/Dashboard/MyProfile.js
@@ -1,27 +1,42 @@
-import { async } from '@firebase/util';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const MyProfile = () => {
 
     let [user] = useAuthState(auth);
     console.log(user);
+    let email = user?.email;
 
-    let [newProfile, setNewProfile] = useState();
+    let urlEmail = `http://localhost:5000/users/${email}`;
+    let urle = "http://localhost:5000/users";
 
+    let { data: users, isLoading, refetch } = useQuery('users', () => fetch(urlEmail, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            // authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading> </Loading>
+    }
 
     let updateProfile = async event => {
         event.preventDefault();
 
-        let name = user.displayName ;
-        let email = user.email;
+        let name = user.displayName;
         let profession = event.target.profession.value;
         let about = event.target.about.value;
         let gender = event.target.gender.value;
         let education = event.target.education.value;
-        let number = event.target.number.value;
+        let number = event.target.num.value;
         let address = event.target.address.value;
+        let img = event.target.img.value;
 
         let data = {
             name: name,
@@ -31,29 +46,39 @@ const MyProfile = () => {
             gender: gender,
             education: education,
             number: number,
-            address: address
+            address: address,
+            img: img,
         }
-
-        
-        await setNewProfile(data)
-
-        console.log(data)
-
-
+        let url = `http://localhost:5000/users/${email}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                if (result.modifiedCount > 0 || result.upsertedCount > 0) {
+                    refetch();
+                    toast.success(`Updated User Information`);
+                }
+            })
+            event.target.reset();
     }
 
     return (
         <div className='flex justify-evenly items-center'>
             <div>
                 <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+                    <figure><img src={users?.img} alt="User Image" /></figure>
                     <div class="card-body">
                         <h2 class="card-title font-bold font-mono">
                             {user?.displayName}
-                            {/* <div class="badge badge-secondary">NEW</div> */}
                         </h2>
-                        <p className='font-semibold'> {newProfile?.profession} </p>
-                        <p> {newProfile?.about} </p>
+                        <p className='font-semibold'> {users?.profession} </p>
+                        <p> {users?.about} </p>
                         <div class=" border-2 p-2 rounded-2xl ">
 
                             <div className=' card-actions justify-between'>
@@ -78,19 +103,19 @@ const MyProfile = () => {
                     <div className='flex justify-around font-mono'>
                         <div>
                             <p className=' mt-3 pt-4'> Name: {user?.displayName} </p>
-                            <p className=' mt-3 pt-4'> Gender: {newProfile?.gender} </p>
-                            <p className=' mt-3 pt-4'> Address: {newProfile?.address} </p>
+                            <p className=' mt-3 pt-4'> Gender: {users?.gender} </p>
+                            <p className=' mt-3 pt-4'> Address: {users?.address} </p>
                         </div>
                         <div>
-                            <p className=' mt-3 pt-4'> Education: {newProfile?.education} </p>
-                            <p className=' mt-3 pt-4'> Number: {newProfile?.number}  </p>
+                            <p className=' mt-3 pt-4'> Education: {users?.education} </p>
+                            <p className=' mt-3 pt-4'> Number: {users?.number}  </p>
                             <p className=' mt-3 pt-4'> Email: {user?.email} </p>
                         </div>
                     </div>
 
 
                     <div>
-                        <label for="profile" class="btn modal-button w-full max-w-xs ">Update Profile</label>
+                        <label for="profile" class="btn modal-button w-2/4 max-w-xs my-10 ">Update Profile</label>
 
                         <input type="checkbox" id="profile" class="modal-toggle" />
                         <div class="modal modal-bottom sm:modal-middle">
@@ -115,8 +140,8 @@ const MyProfile = () => {
                                 </div>
 
                                 <div class="relative z-0 w-full mb-6 group border-2">
-                                    <label htmlFor="number" className='underline'> Phone Number </label>
-                                    <input type="number" name="number" id="number" className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' />
+                                    <label htmlFor="num" className='underline'> Phone Number </label>
+                                    <input type="number" name="num" id="num" className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' />
                                 </div>
 
                                 <div class="relative z-0 w-full mb-6 group border-2">
